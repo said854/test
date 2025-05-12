9b7b079ac5d5ae3b96193bd42ad5d12fda67e575
Said Joukni <sjoukni@e2r13p7.1337.ma>
2025-05-12 22:51:47 +0100

    fv

--- execution/pipe.c
+++ execution/pipe.c
@@ -3,106 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: sjoukni <sjoukni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:00:37 by sjoukni           #+#    #+#             */
-/*   Updated: 2025/05/07 11:13:18 by hakader          ###   ########.fr       */
+/*   Updated: 2025/05/12 22:51:01 by sjoukni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-// static void	exec_first_child(t_cmd *cmd, int *pipe_fd, char **envp)
-// {
-// 	int	fd_in;
-
-// 	set_child_signals();
-// 	if (cmd->infiles)
-// 	{
-// 		fd_in = open(cmd->infiles, O_RDONLY);
-// 		if (fd_in < 0)
-// 		{
-// 			perror("open infiles");
-// 			exit(1);
-// 		}
-// 		dup2(fd_in, STDIN_FILENO);
-// 		close(fd_in);
-// 	}
-// 	dup2(pipe_fd[1], STDOUT_FILENO);
-// 	close(pipe_fd[0]);
-// 	close(pipe_fd[1]);
-// 	execve(cmd->args[0], cmd->args, envp);
-// 	perror("execve");
-// 	exit(1);
-// }
-
-// static void	exec_second_child(t_cmd *next_cmd, int *pipe_fd, char **envp)
-// {
-// 	int (fd_out), (flags);
-// 	set_child_signals();
-// 	dup2(pipe_fd[0], STDIN_FILENO);
-// 	close(pipe_fd[1]);
-// 	close(pipe_fd[0]);
-// 	if (next_cmd->outfiles)
-// 	{
-// 		flags = O_WRONLY | O_CREAT;
-// 		if (next_cmd->append)
-// 			flags |= O_APPEND;
-// 		else
-// 			flags |= O_TRUNC;
-// 		fd_out = open(next_cmd->outfiles, flags, 0644);
-// 		if (fd_out < 0)
-// 		{
-// 			perror("open outfiles");
-// 			exit(1);
-// 		}
-// 		dup2(fd_out, STDOUT_FILENO);
-// 		close(fd_out);
-// 	}
-// 	execve(next_cmd->args[0], next_cmd->args, envp);
-// 	perror("execve");
-// 	exit(1);
-// }
-
-// void	pipex(t_cmd *cmd, char **envp)
-// {
-// 	int		pipe_fd[2];
-// 	pid_t	pid1;
-// 	pid_t	pid2;
-
-// 	if (pipe(pipe_fd) == -1)
-// 	{
-// 		perror("pipe");
-// 		return ;
-// 	}
-// 	pid1 = fork();
-// 	if (pid1 == 0)
-// 		exec_first_child(cmd, pipe_fd, envp);
-// 	pid2 = fork();
-// 	if (pid2 == 0)
-// 		exec_second_child(cmd->next, pipe_fd, envp);
-// 	close(pipe_fd[0]);
-// 	close(pipe_fd[1]);
-// 	waitpid(pid1, NULL, 0);
-// 	waitpid(pid2, NULL, 0);
-// }
-
-#include <sys/wait.h>
 
 void handle_redirections(t_cmd *cmd)
 {
-	int fd = -1;
-	int i = 0;
+	int fd;
+	int i;
 
-	// Handle input redirections
+	// Handle all input files, but dup2 the last one only
+	fd = -1;
+	i = 0;
 	while (cmd->infiles && cmd->infiles[i])
 	{
-		fd = open(cmd->infiles[i], O_RDONLY);
-		if (fd < 0)
+		int tmp_fd = open(cmd->infiles[i], O_RDONLY);
+		if (tmp_fd < 0)
 		{
 			perror(cmd->infiles[i]);
 			exit(EXIT_FAILURE);
 		}
+		if (cmd->infiles[i + 1] == NULL)
+			fd = tmp_fd;
+		else
+			close(tmp_fd);
 		i++;
 	}
 	if (fd != -1)
@@ -111,7 +41,6 @@ void handle_redirections(t_cmd *cmd)
 		close(fd);
 	}
 
-	// Handle output redirections
 	fd = -1;
 	i = 0;
 	while (cmd->outfiles && cmd->outfiles[i])
@@ -122,12 +51,16 @@ void handle_redirections(t_cmd *cmd)
 		else
 			flags |= O_TRUNC;
 
-		fd = open(cmd->outfiles[i], flags, 0644);
-		if (fd < 0)
+		int tmp_fd = open(cmd->outfiles[i], flags, 0644);
+		if (tmp_fd < 0)
 		{
 			perror(cmd->outfiles[i]);
 			exit(EXIT_FAILURE);
 		}
+		if (cmd->outfiles[i + 1] == NULL)
+			fd = tmp_fd;
+		else
+			close(tmp_fd);
 		i++;
 	}
 	if (fd != -1)
@@ -137,9 +70,7 @@ void handle_redirections(t_cmd *cmd)
 	}
 }
 
-
-// void exec_pipeline_cmd(t_cmd *cmd, t_env **env_list, char **envp, char **paths, int in_fd, int out_fd)
-void exec_pipeline_cmd(t_shell *shell, char **paths, int in_fd,
+void exec_pipeline_cmd(t_shell *shell, t_cmd *cmd, char **paths, int in_fd,
 						int out_fd, t_list *alloc_list)
 {
 	char *cmd_path;
@@ -147,26 +78,25 @@ void exec_pipeline_cmd(t_shell *shell, char **paths, int in_fd,
 	set_child_signals();
 	dup2(in_fd, STDIN_FILENO);
 	dup2(out_fd, STDOUT_FILENO);
-	handle_redirections(shell->cmds);
+	handle_redirections(cmd);
 
-	if (!shell->cmds->args || !shell->cmds->args[0])
+	if (!cmd->args || !cmd->args[0])
 		exit(EXIT_SUCCESS);
 
-	if (is_builtin_name(shell->cmds->args[0]))
+	if (is_builtin_name(cmd->args[0]))
 		exit(exec_builtin(&shell, alloc_list));
 
-	cmd_path = check_cmd(paths, shell->cmds->args[0], alloc_list);
+	cmd_path = check_cmd(paths, cmd->args[0], alloc_list);
 	if (!cmd_path)
 	{
-		fprintf(stderr, "%s: command not found\n", shell->cmds->args[0]);
+		fprintf(stderr, "%s: command not found\n", cmd->args[0]);
 		exit(127);
 	}
-	execve(cmd_path, shell->cmds->args, shell->envp);
+	execve(cmd_path, cmd->args, shell->envp);
 	perror("execve");
 	exit(EXIT_FAILURE);
 }
 
-// void pipex(t_cmd *cmd, t_env *env_list, char **envp)
 void pipex(t_shell **shell, t_list *alloc_list)
 {
 	int		pipe_fd[2];
@@ -187,7 +117,7 @@ void pipex(t_shell **shell, t_list *alloc_list)
 		if (pid == 0)
 		{
 			close(pipe_fd[0]);
-			exec_pipeline_cmd((*shell), paths, prev_fd, pipe_fd[1], alloc_list);
+			exec_pipeline_cmd(*shell, current, paths, prev_fd, pipe_fd[1], alloc_list);
 		}
 		close(pipe_fd[1]);
 		if (prev_fd != STDIN_FILENO)
@@ -196,20 +126,14 @@ void pipex(t_shell **shell, t_list *alloc_list)
 		current = current->next;
 	}
 
-	// Last command in pipeline
 	if (current)
 	{
 		pid = fork();
 		if (pid == 0)
-			 exec_pipeline_cmd((*shell), paths, prev_fd, STDOUT_FILENO, alloc_list);
+			exec_pipeline_cmd(*shell, current, paths, prev_fd, STDOUT_FILENO, alloc_list);
 		if (prev_fd != STDIN_FILENO)
 			close(prev_fd);
 	}
-	// int status, exit_code;
-	// waitpid(pid, &status, 0);
-	// if (WIFEXITED)
-	// 	exit_code = WEXITSTATUS(status);
 	while (wait(NULL) > 0)
 		;
-	// free_array(paths);
 }
